eef35f8148edc0e915cff2ecb68b7c329d7403de
Your Name <you@example.com>
2025-12-17 18:55:52 +0500

    update middleware

--- middleware/adminAuth.js
+++ middleware/adminAuth.js
@@ -11,7 +11,7 @@ module.exports = function (req, res, next) {
     const token = authHeader.split(" ")[1];
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    console.log("DECODED TOKEN:", decoded); // 👈 ADD THIS TEMP
+    console.log("DECODED TOKEN:", decoded); 
 
     if (decoded.role !== "admin") {
       return res.status(403).json({ message: "Not an admin" });
