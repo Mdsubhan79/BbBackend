4c7ed2c8fcf6db39dbda7c2b41ffa12b796d3d80
Your Name <you@example.com>
2026-02-13 12:20:25 +0500

    update

--- models/Tiffin.js
+++ models/Tiffin.js
@@ -1,30 +1,12 @@
 const mongoose = require("mongoose");
 
 const tiffinSchema = new mongoose.Schema({
-  planName: {
-    type: String,
-    required: true
-  },
-  type: {
-    type: String,
-    enum: ["veg", "nonveg"],
-    required: true
-  },
-  mealTime: {
-    type: [String],   
-    required: true
-  },
-  description: {
-    type: String
-  },
-  price: {
-    type: Number,
-    required: true
-  },
-  active: {
-    type: Boolean,
-    default: true
-  }
+  planName: { type: String, required: true },
+  type: { type: String, enum: ["veg", "nonveg"], required: true },
+  mealTime: { type: [String], required: true },
+  description: { type: String },   
+  price: { type: Number, required: true },
+  active: { type: Boolean, default: true }
 }, { timestamps: true });
 
 module.exports = mongoose.model("Tiffin", tiffinSchema);
