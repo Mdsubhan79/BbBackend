59752b356e95116347d3b2862c58d651560cf0da
Your Name <you@example.com>
2026-05-16 11:19:13 +0500

    update

--- server.js
+++ server.js
@@ -16,6 +16,12 @@ const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 const PORT = process.env.PORT || 5000;
 
+app.listen(PORT, "0.0.0.0", () => {
+
+    console.log(`🚀 Server running on port ${PORT}`);
+
+});
+
 
 const clients = new Set();
 
